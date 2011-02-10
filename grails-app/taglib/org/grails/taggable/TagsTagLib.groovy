04ec3a0d1fb6899d0ceb467d78baf012c8cc1a51
Peter Ledbrook <peter@cacoethes.co.uk>
2011-02-10 16:05:20 +0000

    Added new tag cloud that doesn't rely on RichUI (and uses styles)

--- grails-app/taglib/org/grails/taggable/TagsTagLib.groovy
+++ grails-app/taglib/org/grails/taggable/TagsTagLib.groovy
@@ -5,15 +5,65 @@ import grails.util.*
 class TagsTagLib {
     
     static namespace = 'tags'
+    static defaultCssClasses = ['smallest', 'small', 'medium', 'large', 'largest']
     
+    def grailsApplication
     def taggableService
     
     def cloud = { attrs ->
+        log.warn "The <tags:cloud> tag is now deprecated in favour of <tags:tagCloud>"
+
         def type = attrs.remove('type')
         // all remaining attributes will be passed through to the richui tag
         attrs.values = taggableService.getTagCounts(type)
         out << plugin.isAvailable(name:'richui') {
-			out << richui.tagCloud(attrs)
-		}
+            out << richui.tagCloud(attrs)
+        }
     }
+
+    /**
+     * Generates a tag cloud using CSS styles to identify the relative importance
+     * of each tag. These CSS styles can be configured via a 'grails.taggable.css.classes'
+     * runtime config setting.
+     */
+    def tagCloud = { attrs ->
+        if (!attrs.action) throwTagError("Required attribute [action] is missing")
+        if (!attrs.tags) throwTagError("Required attribute [tags] is missing")
+        if (!(attrs instanceof Map)) throwTagError("Required attribute [tags] must be a map of tag names to tag counts")
+
+        def classes = grailsApplication.config.grails.taggable.css.classes ?: defaultCssClasses
+
+        // The named arguments for the 'link' GSP tag used to display each tag.
+        def idProperty = attrs?.idProperty ?: "id"
+        def paramsMap = [:]
+        def linkArgs = [action: attrs.action, params: paramsMap]
+
+        // If a controller name is specified, we use that. Otherwise, we leave it
+        // to the 'link' GSP tag to decide whcih controller to use.
+        if (attrs.controller) linkArgs["controller"] = attrs.controller
+
+        // How many times has the most used tag been applied?
+        def maxCount = attrs.tags.values().max()
+        out << "<ol class=\"tagCloud\">"
+        for (t in attrs.tags) {
+            def tagRanking
+            if (t.value == maxCount) {
+                tagRanking = classes.size() - 1
+            }
+            else {
+                // This calculation only works if t.value != maxCount, otherwise
+                // we end up with an array index that is equal to the length of
+                // the 'classes' list.
+                tagRanking = (classes.size() * t.value).intdiv(maxCount)
+            }
+
+            out << "<li class=\"${classes[tagRanking]}\">"
+
+            paramsMap[idProperty] = t.key
+            out << g.link(linkArgs.clone(), t.key)
+            out << "</li>"
+        }
+        out << "</ol>"
+    }
+    
 }
---
+++ test/unit/org/grails/taggable/TagsTagLibUnitTests.groovy
@@ -0,0 +1,87 @@
+package org.grails.taggable
+
+import grails.test.TagLibUnitTestCase
+import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
+
+class TagsTagLibUnitTests extends TagLibUnitTestCase {
+    def currentIdProperty = "id"
+
+    void setUp() {
+        super.setUp()
+
+        def mockTags = mockFor(tagLib.class)
+        mockTags.demand.link(0..100) { Map args, String body ->
+            def controller = args.controller ?: "default"
+            return "<a href='/${controller}/${args.action}/${args.params[currentIdProperty]}'>${body}</a>"
+        }
+
+        tagLib.grailsApplication = new Expando(config: new ConfigObject())
+    }
+
+    void testTagCloudWithNoActionAttribute() {
+        def msg = shouldFail(GrailsTagException) {
+            tagLib.tagCloud tags: [:]
+        }
+
+        assert msg == "Required attribute [action] is missing"
+    }
+
+    void testTagCloudWithNoTagsAttribute() {
+        def msg = shouldFail(GrailsTagException) {
+            tagLib.tagCloud action: "browseByTag"
+        }
+
+        assert msg == "Required attribute [tags] is missing"
+    }
+
+    void testTagCloudWithNoControllerAttribute() {
+        tagLib.tagCloud tags: [hello: 1, world: 10, apple: 3, orange: 7], action: "byTag"
+
+        def tagOutput = tagLib.out.toString()
+        assert tagOutput == "<ol class=\"tagCloud\"><li class=\"smallest\">" + 
+                "<a href='/default/byTag/hello'>hello</a></li>" +
+                "<li class=\"largest\"><a href='/default/byTag/world'>world</a></li>" +
+                "<li class=\"small\"><a href='/default/byTag/apple'>apple</a></li>" +
+                "<li class=\"large\"><a href='/default/byTag/orange'>orange</a></li>" +
+                "</ol>"
+    }
+
+    void testTagCloudWithControllerAttribute() {
+        tagLib.tagCloud tags: [hello: 1, world: 10, apple: 3, orange: 7], controller: "plugin", action: "byTag"
+
+        def tagOutput = tagLib.out.toString()
+        assert tagOutput == "<ol class=\"tagCloud\"><li class=\"smallest\">" + 
+                "<a href='/plugin/byTag/hello'>hello</a></li>" +
+                "<li class=\"largest\"><a href='/plugin/byTag/world'>world</a></li>" +
+                "<li class=\"small\"><a href='/plugin/byTag/apple'>apple</a></li>" +
+                "<li class=\"large\"><a href='/plugin/byTag/orange'>orange</a></li>" +
+                "</ol>"
+    }
+
+    void testTagCloudWithCustomIdProperty() {
+        currentIdProperty = "tagName"
+        tagLib.tagCloud tags: [hello: 1, world: 10, apple: 3, orange: 7], controller: "plugin", action: "byTag", idProperty: "tagName"
+
+        def tagOutput = tagLib.out.toString()
+        assert tagOutput == "<ol class=\"tagCloud\"><li class=\"smallest\">" + 
+                "<a href='/plugin/byTag/hello'>hello</a></li>" +
+                "<li class=\"largest\"><a href='/plugin/byTag/world'>world</a></li>" +
+                "<li class=\"small\"><a href='/plugin/byTag/apple'>apple</a></li>" +
+                "<li class=\"large\"><a href='/plugin/byTag/orange'>orange</a></li>" +
+                "</ol>"
+    }
+
+    void testTagCloudWithCustomCssClasses() {
+        tagLib.grailsApplication.config.grails.taggable.css.classes = [ "one", "two", "three", "four" ]
+        currentIdProperty = "tagName"
+        tagLib.tagCloud tags: [hello: 2, world: 10, apple: 3, orange: 7], controller: "plugin", action: "byTag", idProperty: "tagName"
+
+        def tagOutput = tagLib.out.toString()
+        assert tagOutput == "<ol class=\"tagCloud\"><li class=\"one\">" + 
+                "<a href='/plugin/byTag/hello'>hello</a></li>" +
+                "<li class=\"four\"><a href='/plugin/byTag/world'>world</a></li>" +
+                "<li class=\"two\"><a href='/plugin/byTag/apple'>apple</a></li>" +
+                "<li class=\"three\"><a href='/plugin/byTag/orange'>orange</a></li>" +
+                "</ol>"
+    }
+}
